bbf9f80d2c766a0aed8189766c73f8c9bc1edb09
root <root@ubuntu1.0>
2023-01-13 08:55:31 +0000

    devops2.0

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 resource "aws_instance" "site" {
     ami = "ami-0f69bc5520884278e"
     instance_type="t2.micro"
-    key_name="diffender-1"
+    key_name="terraform-key"
     vpc_security_group_ids = ["sg-0e52a025ddf2cbc05"]
         tags = {
             Name = var.name
