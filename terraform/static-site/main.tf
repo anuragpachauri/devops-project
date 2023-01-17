deebf4c3f5d6ee1d0447e4b401a41e805aaeb083
root <root@ubuntu1.0>
2023-01-17 09:52:41 +0000

    devops4.0

--- terraform/static-site/main.tf
+++ terraform/static-site/main.tf
@@ -10,6 +10,7 @@ resource "aws_instance" "site" {
     instance_type="t2.micro"
     key_name="anurag"
     subnet_id = "subnet-06d5d1e047ff02f83"
+    associate_public_ip_address = true
     vpc_security_group_ids = ["sg-04ec6643f9f06e221"]
         tags = {
             Name = var.name
